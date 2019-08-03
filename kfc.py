d647f28a270e891c65fd551438706b89277a7993
Manjuguna <52068092+Manjuguna@users.noreply.github.com>
2019-08-03 15:13:09 +0530

    Create kfc.py

---
+++ kfc.py
@@ -0,0 +1,17 @@
+Art1,Bat=map(int,input().split())
+Cat=list(map(int,input().split()))
+purt=list(map(int,input().split()))
+qurt=[]
+aat=0
+for i in range(Art1):
+    xt=purt[i]/Cct[i]
+    qurt.append(xt)
+while Bat>=0 and len(qurt)>0:
+    mindex=qurt.index(max(qurt))
+    if Bat>=Cat[mindex]:
+        aat=aat+purt[mindex]
+        Bat=Bat-Cat[mindex]
+    Cat.pop(mindex)
+    purt.pop(mindex)
+    qurt.pop(mindex)
+print(aat)
